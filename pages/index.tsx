e4e537a74f0fc380c9a39ae67e5c52265fb5d7c1
Bruno de Araújo Alves <me@baraus.dev>
2021-07-02 11:48:02 -0300

    Swap position skills and jobs

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,8 +14,8 @@ export default function Home() {
 		<React.Fragment>
 			<StatusBlock />
 			<HeroBlock />
-			<LeaderBoardSkills/>
 			<LeaderBoardJobs/>
+			<LeaderBoardSkills/>
 			{/*<LogoBlock />*/}
 			{/*<PricingBlock />*/}
 			{/*<RatingBlock />*/}
