bb4911d8c50e6dac7da13f1e06e5948c28a6dc83
Bruno de Araújo Alves <me@baraus.dev>
2021-07-17 11:18:52 -0300

    Update HeaderBlock.tsx

--- components/blocks/HeaderBlock.tsx
+++ components/blocks/HeaderBlock.tsx
@@ -19,7 +19,7 @@ const HeaderBlock = () => {
 	return (
 		<>
 			<header className='max-w-screen-xl mx-auto px-6 lg:px-8 xl:px-4 py-4 lg:py-6 flex justify-between'>
-				<div className='w-full md:w-1/3'>
+				<div className='min-w-[180px]'>
 					<Link href='/'>
 						<a className='text-2xl font-bold font-minecrafter-alt tracking-wider'>
 							Comp <span className='text-amber-400'>Craft</span>
@@ -28,29 +28,29 @@ const HeaderBlock = () => {
 
 				</div>
 
-				<label htmlFor='nav-mobile' className='md:hidden'><FontAwesomeIcon icon={['fas', 'bars']}
+				<label htmlFor='nav-mobile' className='lg:hidden'><FontAwesomeIcon icon={['fas', 'bars']}
 																																					 className='text-2xl' /></label>
 				<input
 					type='checkbox'
 					id='nav-mobile'
 					className='hidden' />
 				<nav
-					className='flex absolute top-[-200%] text-lg flex-col left-0 px-6 py-2 bg-white z-10 w-screen nav-collapsable transition-all ease-in duration-200 md:static  md:flex-row md:gap-4 md:justify-end md:p-0 md:m-0 md:w-full'>
+					className='flex absolute top-[-200%] text-lg flex-col left-0 px-6 py-2 bg-white z-10 w-screen nav-collapsable transition-all ease-in duration-200 lg:static  lg:flex-row lg:gap-4 lg:justify-end lg:p-0 lg:m-0 lg:w-full lg:items-center'>
 					{
 						links.map(item => (
 							<Link key={item.url} href={item.url}>
 								<a
-									className={(router.pathname === item.url ? 'font-semibold' : 'text-gray-500') + ' flex gap-2 items-center hover:text-gray-700 mt-2 md:mt-0 mt-2 md:mt-0'}>
+									className={(router.pathname === item.url ? 'font-semibold' : 'text-gray-500') + ' flex gap-2 items-center hover:text-gray-700 mt-2 lg:mt-0 mt-2 lg:mt-0'}>
 									{item.label}
 								</a>
 							</Link>
 						))
 					}
 					<a href='https://tlauncher.org/' target='_blank'
-						 className='text-gray-500 flex items-center hover:text-gray-700 mt-2 md:mt-0'
+						 className='text-gray-500 flex items-center hover:text-gray-700 mt-2 lg:mt-0'
 						 title='Download Minecraf'>TLauncher</a>
 					<a href='https://discord.gg/ECkXBMZQ6T' target='_blank'
-						 className='text-gray-500 flex items-center hover:text-gray-700 mt-2 md:mt-0'>Discord</a>
+						 className='text-gray-500 flex items-center hover:text-gray-700 mt-2 lg:mt-0'>Discord</a>
 					<a target='_blank'
 						 style={{
 							 background: '#434d58',
@@ -59,7 +59,7 @@ const HeaderBlock = () => {
 							 fontSize: '16px',
 							 textShadow: '0 1px rgba(0, 0, 0, 0.3)',
 						 }}
-						 className='text-white flex items-center hover:text-gray-200 items-center justify-center shadow px-3 py-1 rounded mt-2 md:mt-0'
+						 className='text-white flex items-center hover:text-gray-200 items-center justify-center shadow px-3 py-1 rounded mt-2 lg:mt-0'
 						 href='https://donorbox.org/comp-craft-donation'>
 						<img src='https://donorbox.org/images/red_logo.png' alt='' className='object-contain mr-2' />
 						<span>Donate</span>
